7fe9cd549fb77d445ff528cc19b1b1cc8621d57a
Chen Fishbein <cf27675@gmail.com>
2016-05-18 12:49:20 +0300

    removed redundant files

--- CN1Bluethooth/native/ios/com_codename1_cordova_CordovaImpl.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface com_codename1_cordova_CordovaImpl : NSObject {
-}
-
--(BOOL)execute:(NSString*)param param1:(NSString*)param1;
--(BOOL)isSupported;
-@end
