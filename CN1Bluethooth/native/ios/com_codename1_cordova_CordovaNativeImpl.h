a3e388b4e32b6fbee18e9c2179c9d008161f74cc
Steve Hannah <steve@weblite.ca>
2016-05-27 16:38:33 -0700

    Added iOS support

--- CN1Bluethooth/native/ios/com_codename1_cordova_CordovaNativeImpl.h
+++ CN1Bluethooth/native/ios/com_codename1_cordova_CordovaNativeImpl.h
@@ -1,6 +1,9 @@
 #import <Foundation/Foundation.h>
+#import "BluetoothLePlugin.h"
 
 @interface com_codename1_cordova_CordovaNativeImpl : NSObject {
+    
+    BluetoothLePlugin* _bluetoothPlugin;
 }
 
 -(BOOL)execute:(NSString*)param param1:(NSString*)param1;
