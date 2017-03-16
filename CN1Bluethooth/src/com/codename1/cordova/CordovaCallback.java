9466008afd5053f9fd9f96017cc2392a88336ab8
Chen Fishbein <cf27675@gmail.com>
2017-03-16 16:01:32 +0200

    fixed EDT issue

--- CN1Bluethooth/src/com/codename1/cordova/CordovaCallback.java
+++ CN1Bluethooth/src/com/codename1/cordova/CordovaCallback.java
@@ -7,6 +7,7 @@ package com.codename1.cordova;
 
 import ca.weblite.codename1.json.JSONException;
 import ca.weblite.codename1.json.JSONObject;
+import com.codename1.ui.Display;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 
@@ -34,14 +35,22 @@ public class CordovaCallback {
     public void onSuccess(JSONObject json){
         this.response = json;
         if(listener != null){
-            listener.actionPerformed(new ActionEvent(json));
+            Display.getInstance().callSerially(new Runnable(){
+                public void run(){
+                    listener.actionPerformed(new ActionEvent(json));
+                }
+            });
         }
     }
 
     public void sendResult(JSONObject json){
         this.response = json;
         if(listener != null){
-            listener.actionPerformed(new ActionEvent(json));
+            Display.getInstance().callSerially(new Runnable(){
+                public void run(){
+                    listener.actionPerformed(new ActionEvent(json));
+                }
+            });
         }
     }
 
